71d8495009e4793b6c547151d407eae44d544cea
root <root@localhostobsidianrook.com>
2004-02-10 18:52:31 -0800

    Made a stab at implementing this fillip: "perhaps it should report [No completion]" Got it working as well as it's going to, I'm going to rip it out. Not worth polishing it up. Originally RCS check-in revision 1.44 on 2004.02.10.19.52.31

--- elisp/perlnow.el
+++ elisp/perlnow.el
@@ -5,7 +5,7 @@
 ;; Copyright 2004 Joseph Brenner
 ;;
 ;; Author: doom@kzsu.stanford.edu
-;; Version: $Id: perlnow.el,v 1.43 2004/02/10 08:50:32 doom Exp root $
+;; Version: $Id: perlnow.el,v 1.44 2004/02/10 19:52:31 doom Exp root $
 ;; Keywords: 
 ;; X-URL: http://www.grin.net/~mirthless/perlnow/
 
@@ -930,26 +930,35 @@ defaults to using the module root of the current file buffer."
 ;;; then tabby and spacey become simple wrappers.
 
 ;;; TODO 
-;;; Ideally it should report [No completion] or something, 
-;;; which means that " [blah]" stuff should be detected and ignored 
-;;; in the input here.  (low priority)
+;;; (a) Ideally perhaps it should report [No completion] or something.  
+;;; (b) Which means that " [blah]" stuff should be detected and ignored 
+;;;     in the input here.  ((Part b is DONE))
+;;; (low priority)
+;;; Actually, I gave this a try, and I don't see how to get it to work. 
+;;; You need the bracketed comment to be inaccessible to the user, 
+;;; it should go away the moment you start typing.  Redefine every key 
+;;; to a special self-insert wrapper?  Look for hooks on self-insert to 
+;;; blank it out before adding a new character?  This is too much trouble 
+;;; for a *very* minor addition of functionality.  Drop it.
 
   (interactive)
   (let* ( ; empty declarations:
-         file-system-path-pat new-stuff-starts new-stuff result
-         candidate-alist file-list suggested-completion
-         field-start two-pieces-list perlish-path fragment fragment-pat file-system-path
-         lastchar
-         return
+         candidate-alist suggested-completion
+         field-start field-end
+         two-pieces-list perlish-path fragment fragment-pat file-system-path
+         lastchar return result
+         comment-to-user
           ; assignments setq's in all but name:
          (raw_string (buffer-string))
          (end-of-prompt-pat ": ")
+         (start-of-comment-pat "  \\[") ; Though I bet this is already defined (dynamic scoping is weird).
          (pm-extension-pat "\\.pm$")
          )
 
          (string-match end-of-prompt-pat raw_string)
          (setq field-start (match-end 0)) ; also used later to blank minibuffer
-         (setq minibuffer-string (substring raw_string field-start))
+         (setq field-end (string-match start-of-comment-pat raw_string))
+         (setq minibuffer-string (substring raw_string field-start field-end))
 
          ; No single trailing colons allowed: silently double them up
          (if (string-match "[^:]:$" minibuffer-string)
@@ -968,28 +977,30 @@ defaults to using the module root of the current file buffer."
          (setq candidate-alist (perlnow-list-directory-as-alist file-system-path fragment-pat))
          (setq returned (try-completion fragment candidate-alist))
 
-         (if (string-match "::" perlish-path) 
+         (if (string-match "::" perlish-path) ; Are we inside the perl package namespace yet?
              (setq separator "::")
            (setq separator "/"))
 
          (cond ((eq returned nil)
-                (setq suggested-completion fragment)) 
-               ((eq returned t)
-                (if (string-match pm-extension-pat fragment)
+                (setq suggested-completion fragment)
+                (setq comment-to-user "  [no completions]") )
+               ((eq returned t) ; precise match that is not a *.pm file is a directory, add separator
+                 (if (string-match pm-extension-pat fragment)
                     (setq suggested-completion (substring fragment 0 (match-beginning 0) ))
-                  (setq suggested-completion (concat fragment separator))
-                  )
-                )
+                  (setq suggested-completion (concat fragment separator)))) 
                (t
                  (setq suggested-completion returned)))
-         ; yeah, checking this *again* is inelegant, but WTH
-         (if (string-match pm-extension-pat suggested-completion)
+         
+         ; Prevents .pm extensions from appearing in the minibuffer
+         (if (string-match pm-extension-pat suggested-completion) ; Yeah, checking *again*. Inelegant, but WTH
              (setq suggested-completion (substring suggested-completion 0 (match-beginning 0) )))
 
          (setq result (concat perlish-path suggested-completion))
 
          (if (string= result minibuffer-string) ; if there's no change from the input value, go into help
              (perlnow-read-minibuffer-completion-help))
+
+         (setq result (concat result comment-to-user))
 
          (delete-region (+ 1 field-start) (point-max)) ; blank minibuffer 
          (insert result) 
@@ -1168,7 +1179,8 @@ associated with them are sequential numbers"
 (defun readem ()
   "and weep, most likely"
   (interactive)
-  (let (  (init   nil) ; 
+  (let ((start-of-comment-pat "  \\[") ; Maybe this should be global, but WTF.
+        (init   nil) ; 
         (keymap perlnow-read-minibuffer-map) ; Can feed it a keymap!  
         (read   nil) ; 
         (hist   nil) ; 
@@ -1179,6 +1191,12 @@ associated with them are sequential numbers"
   (setq result
         (read-from-minibuffer "Give it to me: " 
                               init keymap read hist def iim))
+
+  ; strip any trailing comments in brackets 
+  ; (do I have to do *everything* around here?)
+  (setq result 
+        (substring result 0 (string-match start-of-comment-pat result)))
+
   (message "result: %s" result)
    ))
 
