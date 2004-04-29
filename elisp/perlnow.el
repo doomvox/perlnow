552efd04834d6c07a81ce5ad2bc241aef6a6a720
root <root@localhostobsidianrook.com>
2004-04-28 23:51:12 -0700

    Bumping version up to 0.4 (just pushed out 0.3). Originally RCS check-in revision 1.206 on 2004.04.28.23.51.12

--- elisp/perlnow.el
+++ elisp/perlnow.el
@@ -5,7 +5,7 @@
 ;; Copyright 2004 Joseph Brenner
 ;;
 ;; Author: doom@kzsu.stanford.edu
-;; Version: $Id: perlnow.el,v 1.205 2004/04/28 23:28:53 doom Exp root $
+;; Version: $Id: perlnow.el,v 1.206 2004/04/28 23:51:12 doom Exp root $
 ;; Keywords:
 ;; X-URL: http://www.grin.net/~mirthless/perlnow/
 
@@ -37,7 +37,7 @@
 (eval-when-compile
   (require 'cl))
 
-(defconst perlnow-version "0.3"
+(defconst perlnow-version "0.4"
   "The version number of the installed perlnow.el package.
 Check <http://www.grin.net/~mirthless/perlnow/> for the latest.")
 
